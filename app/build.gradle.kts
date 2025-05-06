20564887cdc3e5bcfaf0069a33679fca89478f71
Leon <nonick@nonick.ch>
2025-05-06 21:48:01 +0200

    Add Hotwire Native

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
   implementation(libs.material)
   implementation(libs.androidx.activity)
   implementation(libs.androidx.constraintlayout)
+  implementation(libs.core)
+  implementation(libs.navigation.fragments)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
